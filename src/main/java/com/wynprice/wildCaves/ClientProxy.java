c6c19a290fb4de41efc1258c75f221e1d77ba1dc
Wyn Price <wynprice999@gmail.com>
2017-07-12 21:14:48 +0100

    better name

--- src/main/java/com/wynprice/wildCaves/ClientProxy.java
+++ src/main/java/com/wynprice/wildCaves/ClientProxy.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.ModContainer;
 
 public class ClientProxy extends ServerProxy{
-    private static final String PREFIX = "cavetweaks";
+    private static final String PREFIX = "betterunderground";
     @Override
     public void registerModelBakery() {
     	mb(WildCaves.blockStoneStalactite, WildCaves.stalacs, "stone_");
