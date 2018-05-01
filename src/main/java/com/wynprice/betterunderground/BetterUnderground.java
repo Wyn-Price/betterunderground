fe917ffffe7fc64c19bfe8232b7e44d0cf98ea70
Keny <mylankca1995@gmail.com>
2018-05-01 23:37:35 +0300

    version 1.1.5 to be released

--- src/main/java/com/wynprice/betterunderground/BetterUnderground.java
+++ src/main/java/com/wynprice/betterunderground/BetterUnderground.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = "betterunderground", name = "Better Underground", version = "1.1.4", useMetadata = true)
+@Mod(modid = "betterunderground", name = "Better Underground", version = "1.1.5", useMetadata = true)
 public final class BetterUnderground {
     @SidedProxy(clientSide = "com.wynprice.betterunderground.ClientProxy", serverSide = "com.wynprice.betterunderground.ServerProxy")
     public static ServerProxy proxy;
